08fce8e5b7816892742a2116db51ea5f2d9efbd3
Baron C <cannonbaron@gmail.com>
2021-05-06 21:34:11 -0500

    added prompts for other characters, still need to figure out why inputs are not logged

--- Develop/script.js
+++ Develop/script.js
@@ -11,14 +11,26 @@ function writePassword() {
   
 }
 
+
 var lengthOfPassword = 0
 
-  while (lengthOfPassword < 8 || lengthOfPassword > 128) {
+  if (lengthOfPassword < 8 || lengthOfPassword > 128) {
     lengthOfPassword = parseInt(prompt('length of password (8-128): '))
     if (lengthOfPassword < 8 || lengthOfPassword > 128) {
-      alert('Please enter a number between 8-128')
+      alert('Please enter a number between 8-128');
     }
+  } 
+  if (lengthOfPassword >= 8 && lengthOfPassword <= 128) {
+    confirmNumberCharacter = confirm("Would you numbers in your password");
+    confirmUpperCase = confirm("Would you like upper case letters in your password");
+    confirmLowerCase = confirm("Would you like lower case letters in your password");
+    confirmSpecialCharacter = confirm("Would you like special characters in your password");
   }
+    
+  
+
+  
+  
 
   var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
   var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
@@ -28,13 +40,12 @@ var lengthOfPassword = 0
 
   var confirmLength = "";
   var confirmSpecialCharacter;
-  var confirmNumericCharacter;
+  var confirmNumberCharacter;
   var confirmUpperCase;
   var confirmLowerCase;
 
   var generatePassword = (passwordLength) => {
-    debugger
-    var password = ""
+    
 
     for (let i = 0; i < passwordLength; i++) {
       var randomCharIndex = Math.floor(Math.random() * 26)
